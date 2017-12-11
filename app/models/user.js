6e5f8b00f80b5c67ae4e585a888a51264cc16c23
wesam mustafa <wesam.mustafa100@gmail.com>
2017-12-11 02:19:31 +0200

    setting up basic structure

---
+++ app/models/user.js
@@ -0,0 +1,13 @@
+//get an instance of mongoose and mongoose.schema
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+//setting up our Schema
+var userSchema=new Schema({
+    name:String,
+    password:String,
+    admin:Boolean
+});
+
+// pass mongoose model using module.exports
+var User=mongoose.model(User,userSchema);
+module.exports=User;
