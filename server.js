90f5b7551cd4bc6672693ad7092e28276a87d86b
wesam mustafa <wesam.mustafa100@gmail.com>
2017-12-11 06:44:32 +0200

    Our API is finished

--- server.js
+++ server.js
@@ -49,8 +49,86 @@ app.get('/',function(req,res){
     });
  });
 
-    
+    //get an instance of the router for api routes
+    var apiRoutes=express.Router();
 
+    // route to authenticate a user (POST http://localhost:8080/api/authenticate)
+        apiRoutes.post('/authenticate',function(req,res){
+            //find the user
+            User.findOne({name:req.body.name},function(err,user){
+                if(err) throw err;
+                if(!user){
+                    res.json({success:false,message:'Authentication failed. User not found.'});
+                }else if(user){
+
+                // check if password matches
+
+                if(user.password!= req.body.password){
+                        res.json({success:false,message:'Authentication failed. Wrong password.'});
+                    }else{
+                    // if user is found and password is right
+                    //create a token with only our given payload
+                // we don't want to pass in the entire user since that has the password 
+                        const payload={
+                            admin:user.admin
+                        };
+                        var token =jwt.sign(payload,app.get('superSecret'),{
+                            expiresIn:3600 // expires in 24 hours
+                        });
+
+                        // return the information including token as JSON
+                        res.json({
+                            success:true,
+                            message:'Enjoy your token!',
+                            token:token
+                        });
+                    }
+                }
+            });
+        });
+
+    // route middleware to verify a token
+        apiRoutes.use(function(req, res, next){
+            
+            // check header or url parameters or post parameters for token
+            var token = req.body.token || req.query.token || req.headers['x-access-token'];
+            
+            // decode token 
+            if(token){
+                // verifies secret and checks exp
+                jwt.verify(token,app.get('superSecret'),function(err,decoded){
+                    if(err){
+                        return res.json({success:false,message:'Failed to authenticate token.'});                        
+                    }else {
+                        // if everything is good, save to request for user in other routes
+                        req.decoded=decoded;
+                        next();
+                    }
+                });
+            }else{
+                // if there is no token
+                // return an error 
+                return res.status(403).send({
+                    success:false,
+                    message:'No token provided.'
+                });
+            }
+        });
+        
+    // route to show a random message (GET http://localhost:8080/api/)
+        apiRoutes.get('/',function(req,res){
+            res.json({message:'Welcome to the coolest API on earth!'});
+        });
+
+    // route to return all users (GET http://localhost:8080/api/users)
+        apiRoutes.get('/users',function(req,res){
+            User.find({},function(err,users){
+                if(err) throw err;
+                res.json(users);
+            });
+        });
+    //apply the routes to our application with the prefix /api
+    app.use('/api',apiRoutes);
 //============================
 // start the server ==========
 //============================
